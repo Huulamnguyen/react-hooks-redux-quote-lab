1d128d89f273103566811792166bb4975521ecfb
huulamnguyen <liamnguyen.swe@gmail.com>
2022-03-19 18:32:08 -0400

    commit tree c8b618b4a46b8c328d0ae225e3867b2f4a7f914b

--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -1,9 +1,71 @@
 // Action Creators
 // TODO: Create action creators as defined in tests
 
+export const addQuote = (quote) => {
+  return {
+    type: "quotes/add",
+    payload: quote,
+  }
+}
+
+export const removeQuote = (quoteId) => {
+  return {
+    type: "quotes/remove",
+    payload: quoteId,
+  }
+}
+
+export const upvoteQuote = (quoteId) => {
+  return {
+    type: "quotes/upvote",
+    payload: quoteId
+  }
+}
+
+export const downvoteQuote = (quoteId) => {
+  return {
+    type: "quotes/downvote",
+    payload: quoteId
+  }
+}
+
 // Reducer
 const initialState = [];
 
 export default function quotesReducer(state = initialState, action) {
+  const { type, payload } = action;
+  switch(type){
+    case "quotes/add":
+      return [...state, payload];
+    
+    case "quotes/remove":
+      return state.filter(quote => quote.id !== payload)
+
+    case "quotes/upvote":
+      return state.map(q => {
+        if (q.id === payload) {
+          return {
+            ...q,
+            votes: q.votes + 1
+          }
+        } else {
+          return q
+        }
+      })
+    
+    case "quotes/downvote":
+      return state.map(q => {
+        if(q.id === payload && q.votes > 0) {
+          return {
+            ...q,
+            votes: q.votes - 1
+          }
+        } else {
+          return q
+        }
+      })
+    default:
+      break;
+  }
   return state;
 }
